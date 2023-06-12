e02ff9abeb08682a2b8a70b89be68bb2b2be5a8a
jheiser81 <jheiser81@gmail.com>
2023-06-12 09:16:21 -0700

    Start over

--- C++/Animal(Composition)/Animal(Composition)/Animal.cpp
+++ C++/Animal(Composition)/Animal(Composition)/Animal.cpp
@@ -1,16 +1,2 @@
 #include <iostream>
 #include "Animal.h"
-
-Animal::Animal(const std::string& name, const std::string& sound) : name(name), sound(sound) {}
-
-std::string Animal::getName() const {
-	return name;
-}
-
-void Animal::setName(const std::string& name) {
-	this->name = name;
-}
-
-void Animal::makeSound() {
-	std::cout << sound << std::endl;
-}
--- C++/Animal(Composition)/Animal(Composition)/Animal.h
+++ C++/Animal(Composition)/Animal(Composition)/Animal.h
@@ -1,15 +1,2 @@
 #pragma once
 #include <string>
-
-class Animal {
-protected:
-	std::string name, sound;
-
-public:
-	Animal(const std::string& name, const std::string& sound);
-
-	std::string getName() const;
-	void setName(const std::string& name);
-
-	virtual void makeSound();
-};
