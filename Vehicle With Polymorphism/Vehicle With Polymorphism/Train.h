972c972de62d7685dc443ff74f3626cf850ef129
jheiser81 <jheiser81@gmail.com>
2023-06-13 16:58:41 -0700

    Vehicle base class with derived secondary classes using polymorphism

---
+++ Vehicle With Polymorphism/Vehicle With Polymorphism/Train.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <string>
+#include "Vehicle.h"
+
+class Train : public Vehicle {
+public:
+	Train(const std::string& make, const std::string& model, int year, int numberOfCars);
+
+	void drive() override;
+	void honk() override;
+};
