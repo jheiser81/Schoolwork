cb6ed59293b9413ca204185f4950e1777187ac61
jheiser81 <jheiser81@gmail.com>
2023-06-12 19:20:54 -0700

    Cleaned up

--- C++/Animal(Composition)/Animal(Composition)/Animal.cpp
+++ C++/Animal(Composition)/Animal(Composition)/Animal.cpp
@@ -3,21 +3,11 @@
 
 Animal::Animal(const std::string& name, const std::string& sound) : name(name), sound(sound) {}
 
-std::string Animal::getName() const {
-	return name;
-}
+std::string Animal::getName() const {return name;}
+void Animal::setName(const std::string& name) {this->name = name;}
 
-std::string Animal::getSound() const {
-	return sound;
-}
-
-void Animal::setName(const std::string& name) {
-	this->name = name;
-}
-
-void Animal::setSound(const std::string& sound) {
-	this->sound = sound;
-}
+std::string Animal::getSound() const {return sound;}
+void Animal::setSound(const std::string& sound) {this->sound = sound;}
 
 void Animal::makeSound() const {
 	std::cout << name << " says " << sound << "\n";
